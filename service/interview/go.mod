26c3e424611ee7c477f40ad8c71489bd05fb36cb
RyanHUNGry <ryhung@ucsd.edu>
2025-03-21 13:56:57 -0700

    added logging

--- service/interview/go.mod
+++ service/interview/go.mod
@@ -1,17 +1,26 @@
 module interview
 
-go 1.22.0
+go 1.23.0
+
+toolchain go1.23.7
 
 require (
-	github.com/gocql/gocql v1.7.0 // indirect
-	github.com/golang/snappy v0.0.3 // indirect
+	github.com/gocql/gocql v1.7.0
+	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.3.1
+	github.com/joho/godotenv v1.5.1
+	google.golang.org/grpc v1.71.0
+	google.golang.org/protobuf v1.36.5
+)
+
+require (
+	github.com/go-kit/kit v0.10.0 // indirect
+	github.com/go-kit/log v0.2.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
 	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
-	google.golang.org/grpc v1.71.0 // indirect
-	google.golang.org/protobuf v1.36.4 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250313205543-e70fdf4c4cb4 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 )
--- service/interview/main.go
+++ service/interview/main.go
@@ -15,6 +15,9 @@ import (
 	"os"
 	"path/filepath"
 
+	kitlog "github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
 	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
 )
@@ -73,8 +76,36 @@ func initializeGrpc() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	var opts []grpc.ServerOption
-	grpcServer := grpc.NewServer(opts...)
+	logger := kitlog.NewLogfmtLogger(os.Stdout)
+	interceptorLogger := func(l kitlog.Logger) logging.Logger {
+		// The logging.LoggerFunc type is a typed function implementing logging.Logger interface so type convert ordinary function to be of logging.LoggerFunc
+		return logging.LoggerFunc(func(_ context.Context, lvl logging.Level, msg string, fields ...any) {
+			largs := append([]any{"msg", msg}, fields...)
+			switch lvl {
+			case logging.LevelDebug:
+				_ = level.Debug(l).Log(largs...)
+			case logging.LevelInfo:
+				_ = level.Info(l).Log(largs...)
+			case logging.LevelWarn:
+				_ = level.Warn(l).Log(largs...)
+			case logging.LevelError:
+				_ = level.Error(l).Log(largs...)
+			default:
+				panic(fmt.Sprintf("unknown level %v", lvl))
+			}
+		})
+	}
+
+	var loggingOption []logging.Option = []logging.Option{
+		logging.WithLogOnEvents(logging.StartCall, logging.FinishCall),
+	}
+	var serverOption []grpc.ServerOption = []grpc.ServerOption{
+		grpc.ChainUnaryInterceptor(
+			logging.UnaryServerInterceptor(interceptorLogger(logger), loggingOption...),
+		),
+	}
+
+	grpcServer := grpc.NewServer(serverOption...)
 	interviewServiceImpl := &api.InterviewServiceServerImpl{
 		Database: <-databaseChannel,
 	}
